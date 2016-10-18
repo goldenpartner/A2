4d3ee50eff87558ef9f838816ca3039d5c75fcd8
chhs1129 <chhs1129@gmail.com>
2016-10-17 19:25:20 -0700

    Add files via upload

--- display.jl
+++ display.jl
@@ -1,6 +1,21 @@
-#Standard 9*9
-module table
-  temp_standard=["l0" "n0" "s0" "g0" "k0" "g0" "s0" "n0" "l0";
+temp_standard=["l0" "n0" "s0" "g0" "k0" "g0" "s0" "n0" "l0";
+                " " "b0" " " " " " " " " " " "r0" " ";
+                "p0" "p0" "p0" "p0" "p0" "p0" "p0" "p0" "p0";
+                " " " " " " " " " " " " " " " " " ";
+                " " " " " " " " " " " " " " " " " ";
+                " " " " " " " " " " " " " " " " " ";
+                "p1" "p1" "p1" "p1" "p1" "p1" "p1" "p1" "p1";
+                " " "r1" " " " " " " " " " " "b1" " ";
+                "l1" "n1" "s1" "g1" "k1" "g1" "s1" "n1" "l1";]
+
+temp_mini=["k0" "g0" "s0" "b0" "r0";
+      "p0" " " " " " " " ";
+      " " " " " " " " " ";
+      " " " " " " " " "p1";
+      "r1" "b1" "s1" "g1" "k1";]
+
+function defult_standard()
+  standard=["l0" "n0" "s0" "g0" "k0" "g0" "s0" "n0" "l0";
             " " "b0" " " " " " " " " " " "r0" " ";
             "p0" "p0" "p0" "p0" "p0" "p0" "p0" "p0" "p0";
             " " " " " " " " " " " " " " " " " ";
@@ -9,57 +24,59 @@ module table
             "p1" "p1" "p1" "p1" "p1" "p1" "p1" "p1" "p1";
             " " "r1" " " " " " " " " " " "b1" " ";
             "l1" "n1" "s1" "g1" "k1" "g1" "s1" "n1" "l1";]
-  #rebersed left is (9,9) right is (9,1)
-  temp_mini=["k0" "g0" "s0" "b0" "r0";
+end
+
+function defult_mini()
+  mini=["k0" "g0" "s0" "b0" "r0";
         "p0" " " " " " " " ";
         " " " " " " " " " ";
         " " " " " " " " "p1";
         "r1" "b1" "s1" "g1" "k1";]
-  function defult_standard()
-    standard=["l0" "n0" "s0" "g0" "k0" "g0" "s0" "n0" "l0";
-              " " "b0" " " " " " " " " " " "r0" " ";
-              "p0" "p0" "p0" "p0" "p0" "p0" "p0" "p0" "p0";
-              " " " " " " " " " " " " " " " " " ";
-              " " " " " " " " " " " " " " " " " ";
-              " " " " " " " " " " " " " " " " " ";
-              "p1" "p1" "p1" "p1" "p1" "p1" "p1" "p1" "p1";
-              " " "r1" " " " " " " " " " " "b1" " ";
-              "l1" "n1" "s1" "g1" "k1" "g1" "s1" "n1" "l1";]
-  end
-  function defult_mini()
-    mini=["k0" "g0" "s0" "b0" "r0";
-          "p0" " " " " " " " ";
-          " " " " " " " " " ";
-          " " " " " " " " "p1";
-          "r1" "b1" "s1" "g1" "k1";]
-  end
-  Type="S"
-  matrix=temp_standard
-  kill = []
-  lose = []
-  function display()
-    if (Type=="S")
-        for i=1:9
-          println("+---+---+---+---+---+---+---+---+---+")
-          print("|")
-          for j=0:8
-            print(" ",matrix[i,9-j][1]," |")
+end
+
+Type="S"
+matrix=temp_standard
+function display()
+  if (Type=="S")
+    temp=1
+      for i=1:9
+        println("  +---+---+---+---+---+---+---+---+---+")
+        print(temp," |")
+        temp+=1
+        for j=0:8
+          if (matrix[i,9-j][end]=='1')
+            print(" ")
+            if(string(matrix[i,9-j][1])!="k")
+              print_with_color(:red,string(matrix[i,9-j][1]))
+            else
+              print_with_color(:yellow,string(matrix[i,9-j][1]))
+            end
+            print(" |")
+          else
+            print(" ")
+            if(string(matrix[i,9-j][1])!="k")
+              print_with_color(:green,string(matrix[i,9-j][1]))
+            else
+              print_with_color(:yellow,string(matrix[i,9-j][1]))
+            end
+            print(" |")
           end
-          print("\n")
         end
-        println("+---+---+---+---+---+---+---+---+---+")
-    elseif(Type=="M")
-        for i =1:5
-          println("+---+---+---+---+---+")
-          print("|")
-          for j=0:4
-            print(" ",matrix[i,5-j][1]," |")
-          end
-          print("\n")
-        end
+        print("\n")
+      end
+      println("  +---+---+---+---+---+---+---+---+---+")
+      println("    1   2   3   4   5   6   7   8   9")
+  elseif(Type=="M")
+      for i =1:5
         println("+---+---+---+---+---+")
-    else
-      println("error!")
-    end
+        print("|")
+        for j=0:4
+          print(" ",matrix[i,5-j][1]," |")
+        end
+        print("\n")
+      end
+      println("+---+---+---+---+---+")
+  else
+    println("error!")
   end
 end
