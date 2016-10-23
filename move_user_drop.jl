73a6a587a3294e291f16e6f2c379b3d08d3a2ec5
Oliver5173 <gormory@Outlook.com>
2016-10-23 03:01:20 -0700

    Update move_user_drop.jl

--- move_user_drop.jl
+++ move_user_drop.jl
@@ -2,14 +2,15 @@ using SQLite
 DB = SQLite.DB(ARGS[1])
 #get current move_id
 move_number = length(SQLite.query(DB,"SELECT move_number FROM moves;")[1]) + 1
-token = ARGS[2]
+turn = move_number % 2 == 0 ? "0" : "1"
+token = ARGS[2]*turn
 targetx = ARGS[3]
 targety = ARGS[4]
 SQLite.query(DB,"INSERT INTO moves VALUES($move_number,
                                           \"drop\",
-                                          NULL,
-                                          NULL,
+                                          -1,
+                                          -1,
                                           $targetx,
                                           $targety,
-                                          $token,
+                                          \"$token\",
                                           NULL)")
