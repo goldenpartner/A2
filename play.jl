19f373649a604a667548a0d0cb0689d694c6698e
LKrince <liuhaokun0422@gmail.com>
2016-10-24 14:22:49 -0700

    Add files via upload

--- play.jl
+++ play.jl
@@ -54,9 +54,9 @@ function chess_game()
   run(`julia start.jl $new_game $shogi_type $cheating_mode`)
   # pick side
   println("Enter \"W\" for whith side, Enter \"B\" for black side")
-  side = readline()
+  side = readline()[1]
   println("You chose $side ")
-  if side == "W"
+  if side == 'W'
     side = 0
   else
     side = 1
@@ -64,95 +64,65 @@ function chess_game()
   #start game
   run(`julia --color=yes -- display.jl $new_game`)
 
-  println("your turn : ")
-  println("format: move_type,sourceX,sourceY,targetX,targetY,option")
-  println("move_type can be: move,resign,or drop")
-  println("option can be: b,g,k,l,n,p,r,s, T \(T for promote\)")
-  println("place \"0\" if position is empty")
+
   #get each parameter
   move_number = 1
-
-  move_code = readline()
-  temp = search(move_code,',')
-  move_type = move_code[1:temp-1]
-  println(move_type)
-
-  move_code = move_code[temp+1:end]
-  temp = search(move_code,',')
-  sourceX = parse(move_code[1:temp-1])
-  println(sourceX)
-
-  move_code = move_code[temp+1:end]
-  temp = search(move_code,',')
-  sourceY = parse(move_code[1:temp-1])
-  println(sourceY)
-
-  move_code = move_code[temp+1:end]
-  temp = search(move_code,',')
-  targetX = parse(move_code[1:temp-1])
-  println(targetX)
-
-  move_code = move_code[temp+1:end]
-  temp = search(move_code,',')
-  targetY = parse(move_code[1:temp-1])
-  println(targetY)
-
-  move_code = move_code[temp+1:end]
-  option = move_code
-  println(option)
-
-  while (!win())
+  while (true)
+    println("move_number : $move_number")
     if move_number%2 == side
+      println("your turn:  ")
+      println("format: move_type,sourceX,sourceY,targetX,targetY,option")
+      println("move_type can be: move,resign,or drop")
+      println("option can be: b,g,k,l,n,p,r,s, T \(T for promote\)")
+      println("place \"0\" if position is empty")
+      move_code = readline()
+      temp = search(move_code,',')
+      move_type = move_code[1:temp-1]
+      println(move_type)
+
+      move_code = move_code[temp+1:end]
+      temp = search(move_code,',')
+      sourceX = parse(move_code[1:temp-1])
+      println(sourceX)
+
+      move_code = move_code[temp+1:end]
+      temp = search(move_code,',')
+      sourceY = parse(move_code[1:temp-1])
+      println(sourceY)
+
+      move_code = move_code[temp+1:end]
+      temp = search(move_code,',')
+      targetX = parse(move_code[1:temp-1])
+      println(targetX)
+
+      move_code = move_code[temp+1:end]
+      temp = search(move_code,',')
+      targetY = parse(move_code[1:temp-1])
+      println(targetY)
+
+      move_code = move_code[temp+1:end]
+      option = move_code
+      println(option)
+
       if move_type == "move"
         run(`julia move_user_move.jl $new_game $sourceX $sourceY $targetX $targetY $option`)
       elseif move_type == "drop"
-        run(`julia move_user_resign.jl $new_game T $targetX $targetY`)
+        run(`julia move_user_drop.jl $new_game $option $targetX $targetY`)
       else
-        run(`julia move_user_drop.jl $new_game`)
+        run(`julia move_user_resign.jl $new_game`)
       end
     else
-      run(`julia move.jl $new_game`)
+      println("AI is moving : ")
+      sleep(1)
+      #run(`julia move.jl $new_game`)
     end
+
     run(`julia --color=yes -- display.jl $new_game`)
-
-    move_number++
-    println("your turn:  ")
-    println("format: move_type,sourceX,sourceY,targetX,targetY,option")
-    println("move_type can be: move,resign,or drop")
-    println("option can be: b,g,k,l,n,p,r,s, T \(T for promote\)")
-    println("place \"0\" if position is empty")
-    move_code = readline()
-    temp = search(move_code,',')
-    move_type = move_code[1:temp-1]
-    println(move_type)
-
-    move_code = move_code[temp+1:end]
-    temp = search(move_code,',')
-    sourceX = parse(move_code[1:temp-1])
-    println(sourceX)
-
-    move_code = move_code[temp+1:end]
-    temp = search(move_code,',')
-    sourceY = parse(move_code[1:temp-1])
-    println(sourceY)
-
-    move_code = move_code[temp+1:end]
-    temp = search(move_code,',')
-    targetX = parse(move_code[1:temp-1])
-    println(targetX)
-
-    move_code = move_code[temp+1:end]
-    temp = search(move_code,',')
-    targetY = parse(move_code[1:temp-1])
-    println(targetY)
-
-    move_code = move_code[temp+1:end]
-    option = move_code
-    println(option)
-
+    move_number = move_number+1
   end
 end
 function win(new_game)
+
 end
 
 
