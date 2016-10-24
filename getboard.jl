869dc310af2153e39f2e9f113d31a74ccd68359e
Oliver5173 <gormory@Outlook.com>
2016-10-25 05:36:30 +0800

    Update getboard.jl

--- getboard.jl
+++ getboard.jl
@@ -2,7 +2,7 @@ using SQLite
 
 function getCurrentBoard(DB)
   DB = SQLite.DB(DB)
-  died_token = Array{String}(0)
+  drop_token = Array{String}(0)
   board_type = SQLite.query(DB,"SELECT value FROM meta WHERE key = \"type\";")[1].values[1]
   if board_type == "standard"
     global board = ["l0" "n0" "s0" "g0" "k0" "g0" "s0" "n0" "l0";
@@ -23,7 +23,6 @@ function getCurrentBoard(DB)
   end
   for i = 1:length(SQLite.query(DB,"SELECT move_number FROM moves;")[1])
     global turn = i % 2 == 0 ? "0" : "1"
-    
     move_type = SQLite.query(DB,"SELECT move_type FROM moves WHERE \"move_number\" = $i;")[1].values[1]
     try
       global sourcex = SQLite.query(DB,"SELECT sourcex FROM moves WHERE \"move_number\" = $i;")[1].values[1]
@@ -44,7 +43,7 @@ function getCurrentBoard(DB)
     end
     if move_type == "move"
       if board[targetx,targety] != " "
-        push!(died_token,lowercase(board[targetx,targety]))
+        push!(drop_token,lowercase(string(board[targetx,targety][1])*turn))
         board[targetx,targety] = board[sourcex,sourcey]
         board[sourcex,sourcey] = " "
       else
@@ -55,25 +54,25 @@ function getCurrentBoard(DB)
         board[targetx,targety] = uppercase(board[targetx,targety])
       end
     elseif move_type == "resign"
-      return board,died_token
+      return board,drop_token
     elseif move_type == "drop"
       index = 0
-      for k = 1:length(died_token)
-        if died_token[k] == option * turn
+      for k = 1:length(drop_token)
+        if drop_token[k] == option * turn
           index = k
           break
         end
       end
       if board[targetx,targety] != " "
-        push!(died_token,lowercase(board[targetx,targety]))
+        push!(drop_token,lowercase(string(board[targetx,targety][1])*turn))
       end
       if index != 0
         board[targetx,targety] = option * turn
-        deleteat!(died_token,index)
+        deleteat!(drop_token,index)
       else
         board[targetx,targety] = option * turn
       end
     end
   end
-  return board,died_token
+  return board,drop_token
 end
