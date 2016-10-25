d530f41c51c4e92e3c3a3849b2c3702ed0c2d4ed
Oliver5173 <gormory@Outlook.com>
2016-10-25 08:28:25 +0800

    Update move_user_resign.jl

--- move_user_resign.jl
+++ move_user_resign.jl
@@ -2,4 +2,4 @@ using SQLite
 
 DB = SQLite.DB(ARGS[1])
 move_number = length(SQLite.query(DB,"SELECT move_number FROM moves;")[1]) + 1
-SQLite.execute!(DB,"INSERT INTO moves VALUES($move_number,\"resign\",-1,-1,-1,-1,NULL,NULL)")
+SQLite.execute!(DB,"INSERT INTO moves VALUES($move_number,\"resign\",NULL,NULL,NULL,NULL,NULL,NULL)")
